ef96322d37103732a130e5006b4b6251432de755
James Laska <jlaska@redhat.com>
2011-06-02 14:40:45 -0400

    Stop the build_externals script from being called at scheduler initialization
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>
    Signed-off-by: James Laska <jlaska@redhat.com>

--- scheduler/drone_utility.py
+++ scheduler/drone_utility.py
@@ -64,9 +64,6 @@ class DroneUtility(object):
         if os.path.exists(temporary_directory):
             shutil.rmtree(temporary_directory)
         self._ensure_directory_exists(temporary_directory)
-        build_extern_cmd = os.path.join(results_dir,
-                                        '../utils/build_externals.py')
-        utils.run(build_extern_cmd)
 
 
     def _warn(self, warning):
